59414712d87122bd9949d4b57a9e00fc9224169d
Sebastian Tschan <sebastian.tschan@gmail.com>
2011-04-21 22:49:54 +0900

    Header info comment update.

--- jquery.md5.js
+++ jquery.md5.js
@@ -1,12 +1,12 @@
 /*
  * jQuery MD5 Plugin 1.1
+ * https://github.com/blueimp/jQuery-MD5
  *
- * Copyright 2010, Sebastian Tschan, AQUANTUM
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
  * Licensed under the MIT license:
  * http://creativecommons.org/licenses/MIT/
- *
- * https://blueimp.net
- * http://www.aquantum.de
  * 
  * Based on
  * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
